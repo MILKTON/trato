1d0ed656c9e4a55e30750b57445f67253ec7b690
Milton <mi_correo37@hotmail.com>
2021-01-22 02:04:34 -0600

    Handle route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import Navigation from './Components/Navigation';
 import Footer from './Components/Footer';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Body from './Components/Body';
+import { Route, Switch } from 'react-router-dom';
 import Transfer from './Components/Transfer';
 import Home from './Components/Home';
 import { PrivateRoute } from './_components/';
@@ -10,11 +11,11 @@ function App() {
   return (
     < >
       <Navigation/>
-      <Router>
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/transfer" component={Transfer} />
         <Route path="/login" component={LoginPage} />
-      </Router>
+      </Switch>
       <Footer/>
     </>
   );
--- src/Components/Navigation/index.js
+++ src/Components/Navigation/index.js
@@ -7,27 +7,43 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink,
 } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 const Navigation = () => {
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
-
     return (
       <Navbar color="light" light expand="md">
       <NavbarBrand href="/">Trato</NavbarBrand>
-      <NavbarToggler onClick={toggle} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="mr-auto" navbar>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
           <NavItem>
-            <NavLink href="/">Home</NavLink>
+            <NavLink
+            exact
+            activeClassName="navbar__link--active"
+            className="nav-link"
+            to="/"
+            >
+            Home
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/transfer">Transfer</NavLink>
+            <NavLink
+            exact
+            activeClassName="navbar__link--active"
+            className="nav-link"
+            to="/transfer">
+              Transfer
+            </NavLink>
           </NavItem>
           <NavItem>
-            <NavLink href="/login">Login</NavLink>
+            <NavLink
+            exact
+            activeClassName="navbar__link--active"
+            className="nav-link"
+            to="/login">Login</NavLink>
           </NavItem>
         </Nav>
       </Collapse>
